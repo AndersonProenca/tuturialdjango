7eeb321204272bffa87b67594e970d40366baa3b
Anderson <anderson.proenca1020@gmail.com>
2021-12-17 14:11:03 -0300

    first

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.fields import BigAutoField
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
